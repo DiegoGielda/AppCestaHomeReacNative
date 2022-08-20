cd4dc3149a41339b0eb75b3409239aa79d4779f7
DiegoGielda <diegogielda7@gmail.com>
2022-08-20 16:46:15 -0300

    Criado estrura de boas vindas para o aplicativo

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import { Text, SafeAreaView, StatusBar } from 'react-native';
+import Home from './src/telas/Home';
+
+function App() {
+  return <>
+    <SafeAreaView>
+      <StatusBar />
+      <Home />
+    </SafeAreaView>
+  </>
+
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
